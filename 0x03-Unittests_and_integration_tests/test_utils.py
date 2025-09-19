3535b9369e3ee49df04a317713d91f65f8724bd4
Chukwudi Okolo <chydan14@gmail.com>
2025-09-19 13:24:42 +0100

    Added network testcase

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,9 @@
 """Test module for utils.access_nested_map.
 """
 import unittest
+from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -29,3 +30,20 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(expected_exception) as cm:
             access_nested_map(nested_map, path)
         self.assertEqual(str(cm.exception), f"'{path[-1]}'")
+
+
+class TestGetJson(unittest.TestCase):
+    """Tests the get_json function."""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """Tests that get_json returns the expected result with a mocked HTTP call."""
+        mock_response = unittest.mock.Mock()
+        mock_response.json.return_value = test_payload
+        with patch('requests.get', return_value=mock_response) as mock_get:
+            result = get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
+
