1dbe70edd2ca73ea9e27035cd8f9e59fb00a0243
Chukwudi Okolo <chydan14@gmail.com>
2025-09-21 17:51:28 +0100

    Scaffolding for messaging_app project

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'chats',
 ]
 
 MIDDLEWARE = [
@@ -97,6 +99,18 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+        # You can also add JWT if needed:
+        # "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+}
 
 
 # Internationalization
